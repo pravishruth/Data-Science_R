9f6ab75d100657c9ea8a092cf5d0b2cbb2468923
PRASHANTH KUMAR H R <70260747+pravishruth@users.noreply.github.com>
2020-08-26 21:02:12 +0530

    Add files via upload

---
+++ EG2.R
@@ -0,0 +1,39 @@
+1+1
+2+3*4
+exp(1)
+pi
+X<-58
+Y<-20
+Z<-98
+X*Y*Z
+(21*34)+17/2
+
+my_vector<-12,20,35
+my_vector<-c(12,20,35)
+
+vect_x<-c (2,0,0,4)
+vect_y<-c(1,9,9,9)
+
+# Adding the vector
+vect_x + vect_y
+vect_add<-vect_x+vect_y
+
+#Multiplying vector with some scalar
+vect_x*48
+vect_y*22
+
+#Accessing the elements of the vector 
+vect_x[1]
+vect_y[4]
+
+# Replacement of index
+vect_x[2]<-6
+vect_y[4]<-0
+
+vect_char <- c('R', 'python', 'java', "10","11","20")
+
+df<-data.frame (x=c(1,2,3), y=c("a", "b", "c"))
+df[1,2]
+df[1,1]
+df[1,c(1,2)]
+df[c(1,3),c(1,2)]
